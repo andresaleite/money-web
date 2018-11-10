6994d48ea082a4224a522e8fc47d8b67e1e96677
Andresa <andresaleite@gmail.com>
2018-11-10 15:28:10 -0200

    fim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    nome =  'Andrêsa';
+    data = new Date();
+    preco = 785477;
+    troco = 1002.55;
     funcionarios = [];
 
     aoAdicionar(funcionario : string){
       this.funcionarios.push(funcionario);
     }
+
+
   }
